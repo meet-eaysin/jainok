49ffda093b76e6fd8b7481429ee85d6a8951f13f
meet-eaysin <meet.eaysin@gmail.com>
2026-01-13 11:57:51 +0600

    feat: enhance About and Blog pages with improved content structure and styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,18 @@
 import { Background } from "@/components/background";
 import About from "@/components/blocks/about";
-import { AboutHero } from "@/components/blocks/about-hero";
 
 export default function AboutPage() {
   return (
     <Background>
-      <div className="py-28 lg:py-32 lg:pt-44">
-        <AboutHero />
+      <div className="container py-28 lg:py-32">
+        <h2 className="mb-3 font-serif text-4xl leading-tight font-medium md:text-7xl">
+          About Me
+        </h2>
+        <p className="text-muted-foreground text-lg font-light">
+          My personal philosophy and professional journey.
+        </p>
+      </div>
+      <div className="">
         <About />
       </div>
     </Background>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,6 @@ export default function BlogPage() {
   return (
     <Background className="via-muted to-muted/80">
       <div className="container py-28 lg:py-32">
-        {/* Page Header */}
         <div className="mb-16">
           <h2 className="mb-3 font-serif text-4xl leading-tight font-medium md:text-7xl">
             Blog & Insights
